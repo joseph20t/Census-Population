bf9791a675e8dba460afcade2709dad88bd66b3f
joseph20t <josephgbakutu17@gmail.com>
2021-06-16 10:15:55 +0000

    updated code

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ let district_bar_chart = new Chart(JsChart, {
         backgroundColor: "#B1D2C2",
         
       },
+      {
+        data: [],
+        backgroundColor: "#B1D2C2",
+      },
     ]
   },
   option:{
@@ -36,6 +40,11 @@ let households_Chart = new Chart(household, {
         backgroundColor: "#B1D2C2",
         data: [],
       },
+      { 
+        label: 'Total Population Per County',
+        backgroundColor: "#B1D2C2",
+        data: [],
+      },
     ]
   },
   option:{
@@ -57,6 +66,7 @@ let population_per_households_data = [];
 fetch('census.json')
   .then(response => response.json())
   .then(data => {
+    
     let households_data = data.households;
     population_per_households_data.push(households_data);
 
@@ -65,6 +75,7 @@ fetch('census.json')
 
     let population_data = data.population;
     let households_data_per_county = data.households;
+    josephTest(population_data)
 
     let total_male_population_value = [];
     function getTotalMalePopulation(){
@@ -100,6 +111,7 @@ fetch('census.json')
     }
     getTotalPopulation();
 
+    //gettting the county and their male and female data in an object
     let population_per_county = {}
     function getCountyPopulationValue(){
       population_data.forEach(ele => {
@@ -112,15 +124,16 @@ fetch('census.json')
     }
     getCountyPopulationValue()
     
+    //displaying population data in the chart
     function populatiuonPerCountyChart(){
     let myChart = document.getElementById("myChart");
-    let bar_chart = new Chart(myChart, {
+    let population_chart = new Chart(myChart, {
       type: 'bar',
       data: {
         datasets:[
           { 
             label: 'Total Population Per County',
-            backgroundColor: "#B1D2C2",
+            backgroundColor: "#519872",
             data: population_per_county,
             borderRadius: 10
           },
@@ -142,6 +155,7 @@ fetch('census.json')
     }
     populatiuonPerCountyChart();
 
+    //displaying the male and female data in the doughnut chart
     function doughnutChart(){
       let donut_pie_Chart = document.getElementById("donut_pie_Chart");
       let donut_chart = new Chart(donut_pie_Chart, {
@@ -157,7 +171,7 @@ fetch('census.json')
               
               backgroundColor: [
                 '#FFFFFF',
-                '#519872',  
+                '#B1D2C2',  
               ],
             },
           ],
@@ -194,6 +208,8 @@ fetch('census.json')
       document.querySelector('select').insertAdjacentHTML('beforeend', displaying_county_names);
     })
     getCountyPerDistrict();
+
+    //district population data
     let population_per_district = {}
 
     population_data.forEach(ele => {
@@ -206,11 +222,13 @@ fetch('census.json')
       }
      })  
      
+     //households data removing the duplicate county
      function removeDuplicateCounty(households_data_per_county, key) {
       var check = new Set();
       return households_data_per_county.filter(obj => !check.has(obj[key]) && check.add(obj[key]));
     }
-
+    
+    //inserting in the select tag the county with the district male and female data
     let select_county_per_household = removeDuplicateCounty(households_data_per_county, 'county');
       select_county_per_household.forEach((ele, index) => {
       let displaying_county_names_per_households = `<option value ='${ele.county}'>${ele.county}</optipn> `
@@ -230,10 +248,8 @@ function getCountyPerDistrict(){
   let district_population_per_county_male = [];
   let district_population_per_county_female = [];
   let district_name = [];
-
   storing_of_population_data.forEach(element => {
       element.forEach(ele => {
-        
       if(ele.county === county_value) {
         district_name.push(ele.district)
         district_population_per_county_male.push(ele.male);
@@ -243,47 +259,98 @@ function getCountyPerDistrict(){
 
     })
   })
-  console.log(displayingCountyDistrictData(district_population_per_county_male, district_population_per_county_female, district_name));
+  displayingCountyDistrictData(district_population_per_county_male, district_population_per_county_female, district_name);
 }
 function displayingCountyDistrictData(district_population_per_county_male,district_population_per_county_female, district_name){
   district_bar_chart.data.labels = district_name;
-  
-  district_bar_chart.data.datasets.pop();
+
+  district_bar_chart.data.datasets = [];
   district_bar_chart.data.datasets.push({
-    data: district_population_per_county_male,district_population_per_county_female,
-    label: 'Total Population Per District',
-    backgroundColor: "#B1D2C2",
-    borderRadius: 50
-  }),
-  district_bar_chart.update();
+    data: district_population_per_county_female,
+    label: 'Total Female',
+    // backgroundColor: "lightgray",
+    borderRadius: 20
+  },
+  {
+    data: district_population_per_county_male,
+    label: 'Total Male',
+    backgroundColor: "#519872",
+    borderRadius: 20
+  });
+  
+  district_bar_chart.update()
 }
 
 function getCountyPerhousehold(){
-  let households_population_per_county = [];
-  let county_name = [];
+  let households_male_population = [];
+  let households_female_population = [];
+  let households_population_number = [];
+  let settlement = [];
 
   let household_value = document.getElementById('choosing_county_household_div').value;
   population_per_households_data.forEach(element => {
     element.forEach(ele => {
       if(ele.county === household_value) {
-        
-        county_name.push(ele.county)
-        households_population_per_county.push(ele.male + ele.female);
+        settlement.push(ele.settlement)
+        households_male_population.push(ele.male);
+        households_female_population.push(ele.female);
+        households_population_number.push(ele.household_number)
       } 
     })
 
   })
-  displayingHouseholds(households_population_per_county, county_name)
+ 
+  displayingHouseholds(households_male_population, households_female_population, settlement, households_population_number)
 }
 
-function displayingHouseholds(households_population_per_county, county_name){
-  households_Chart.data.labels = county_name;
+function displayingHouseholds(households_male_population, households_female_population, settlement, households_population_number){
+  households_Chart.data.labels = settlement;
 
-  households_Chart.data.datasets.pop();
+  households_Chart.data.datasets = [];
   households_Chart.data.datasets.push({
-    data: households_population_per_county,
-    label: 'Total Population Per households',
-    backgroundColor: "#B1D2C2",
-  }),
+    data: households_male_population,
+    label: 'Total Households For Male',
+    backgroundColor: "lightgray",
+    borderRadius: 25
+  },
+  {
+    data: households_female_population,
+    label: 'Total Households For Female',
+    backgroundColor: "#519872",
+    borderRadius: 25
+  },
+  {
+    data: households_population_number,
+    label: 'Huseholds Population Number',
+    // backgroundColor: "l",
+    borderRadius: 25
+  }
+  ),
   households_Chart.update();
+}
+
+function josephTest(population_data){
+  // let a = [];
+  // let c = [];
+  population_data.forEach(ele => {
+    // console.log(c)
+    // console.log(c.includes(ele.county))
+    // if(c.includes(ele.county)){
+    //   console.log(ele.county)
+    // }
+    // if(ele.county === "Montserrado "){
+    //   a.push(ele.male)
+     
+    //   let b = a.reduce((acc, cur) => {
+    //     return acc + cur;     
+    //   }, 0)
+      // console.log(c[0])
+      // let t = document.getElementById("hey")
+      // c[0].innerHTML = t
+    // }
+    
+    // console.log(a)
+    // let nest = ele.male
+    // a.push([ele.county] = ele.male)
+  })
 }
